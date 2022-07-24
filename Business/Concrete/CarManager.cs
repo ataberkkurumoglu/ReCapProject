3b0fd98fa17a4838fa8937f52fe747c489ba29e1
Ataberk <kurumoglua@gmail.com>
2022-07-24 17:52:29 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,44 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _cardal;
+
+        public CarManager(ICarDal cardal)
+        {
+            _cardal = cardal;
+        }
+
+        public void Add(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cardal.GetAll();
+        }
+
+        public Car GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal:ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=2,ColorId=3,DailyPrice=15,ModelYear=1998,Description="Volvo"},
+                new Car{Id=2,BrandId=1,ColorId=2,DailyPrice=12,ModelYear=2012,Description="BMW"},
+                new Car{Id=3,BrandId=4,ColorId=1,DailyPrice=123,ModelYear=2022,Description="Mercedes"}
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            var carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+
+        }
+
+        public void Update(Car car)
+        {
+            var carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            car.BrandId = carToUpdate.BrandId;
+            car.ColorId = carToUpdate.ColorId;
+            car.DailyPrice = carToUpdate.DailyPrice;
+            car.Description = carToUpdate.Description;
+            car.ModelYear = carToUpdate.ModelYear;
+        }
+    }
+}
